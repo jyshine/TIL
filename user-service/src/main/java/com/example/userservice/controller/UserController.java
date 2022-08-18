2624a9ef1fa0434bf51a8ae7357e3f788ba97248
june <jyshine3@gmail.com>
2022-08-18 18:11:25 +0900

    feat: User service getUserByAll, getUserById 추가

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.example.userservice.controller;
 
 import com.example.userservice.dto.UserDTO;
+import com.example.userservice.entity.UserEntity;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Gretting;
 import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user-service")
@@ -48,5 +55,27 @@ public class UserController {
         userService.createUser(userDTO);
 
         return "Success Create User";
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<ResponseUser>> getUsers(){
+        Iterable<UserEntity> userList = userService.getUserByAll();
+
+        List<ResponseUser> result = new ArrayList<>();
+        userList.forEach(
+                userEntity -> {
+                    result.add(new ModelMapper().map(userEntity, ResponseUser.class));
+                }
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<ResponseUser> getUser(@PathVariable("userId") String userId){
+        UserDTO userDTO = userService.getUserByUserId(userId);
+        ResponseUser map = new ModelMapper().map(userDTO, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(map);
     }
 }
--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -3,12 +3,16 @@ package com.example.userservice.service;
 import com.example.userservice.dto.UserDTO;
 import com.example.userservice.entity.UserEntity;
 import com.example.userservice.repository.UserRepository;
+import com.example.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -34,5 +38,25 @@ public class UserServiceImpl implements UserService {
         userEntity.setEncryptedPwd(passwordEncoder.encode(userDTO.getPwd()));
 
         return modelMapper.map(userRepository.save(userEntity), UserDTO.class);
+    }
+
+    @Override
+    public UserDTO getUserByUserId(String userId) {
+        UserEntity userEntity = userRepository.findByUserId(userId);
+
+        if (userEntity == null){
+            throw new UsernameNotFoundException("User Not Found");
+        }
+
+        UserDTO userDTO = new ModelMapper().map(userEntity, UserDTO.class);
+        List<ResponseOrder> orderList = new ArrayList<>();
+        userDTO.setOrderList(orderList);
+
+        return userDTO;
+    }
+
+    @Override
+    public Iterable<UserEntity> getUserByAll() {
+        return userRepository.findAll();
     }
 }
