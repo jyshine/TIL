e974a04df06440b89b4ddc19a9a18398b80cc439
june <jyshine3@gmail.com>
2022-09-04 22:30:30 +0900

    feat: algorithm queue add, remove, peek, isEmpty

--- algorithm/java-algorithm/src/main/java/org/example/queue/QueueTest.java
+++ algorithm/java-algorithm/src/main/java/org/example/queue/QueueTest.java
@@ -1,8 +1,11 @@
 package org.example.queue;
+
+import java.util.NoSuchElementException;
+
 class Queue<T> {
     class Node<T> {
         private T data;
-        private Node next;
+        private Node<T> next;
 
         public Node(T data) {
             this.data = data;
@@ -18,11 +21,53 @@ class Queue<T> {
         if (last != null) {
             last.next = t;
         }
+
         last = t;
+
         if (first == null) {
             first = last;
         }
     }
+
+    public T remove() {
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+        T data = first.data;
+        first = first.next;
+
+        if (first == null) {
+            last = null;
+        }
+        return data;
+    }
+
+    public T peek() {
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
 }
 public class QueueTest {
+    public static void main(String[] args) {
+        Queue<Integer> q = new Queue<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.isEmpty());
+        System.out.println(q.remove());
+        System.out.println(q.isEmpty());
+    }
 }
