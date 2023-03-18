34571ce284edc05d9989a0f06d4e0347e0356c1f
june <jyshine3@gmail.com>
2023-03-18 23:28:08 +0900

    feat(webflux): init 초기세팅

---
+++ webflux-sample/src/test/java/com/example/webfluxsample/WebfluxSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
