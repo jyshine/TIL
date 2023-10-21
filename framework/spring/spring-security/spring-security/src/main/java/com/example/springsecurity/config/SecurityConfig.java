777cb9afd710c778f7d5a921b0b2e53eec70c8c9
신준영 <jyshine3@gmail.com>
2023-10-21 23:52:00 +0900

    feat(security): Security config and add securityfilterchain config

--- framework/spring/spring-security/spring-security/src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ framework/spring/spring-security/spring-security/src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -30,4 +30,5 @@ public class SecurityConfig {
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         return daoAuthenticationProvider;
     }
+
 }
---
+++ framework/spring/spring-security/spring-security/src/main/java/com/example/springsecurity/config/SecurityFilterChainConfig.java
@@ -0,0 +1,36 @@
+package com.example.springsecurity.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityFilterChainConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/api/v1/customers")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider);
+//                .addFilterBefore()
+//                ;
+
+        return http.build();
+    }
+}
